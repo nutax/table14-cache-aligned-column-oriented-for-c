4256ff145a02a82d27db31033e1917c1b6b25cf6
Jose <jose.huby@nemolatam.com>
2024-03-30 21:41:56 -0500

    addded comment to explain struct fields

--- table14.h
+++ table14.h
@@ -6,8 +6,8 @@
 
 struct table14_t
 {
-    uint32_t meta[14];
-    void *data;
+    uint32_t meta[14]; // [0] = size, [1-13] = column relative address to first column
+    void *data; // absolute address of first column
 }; // 14 * (4 bytes) +  1 * (8 bytes (or less)) -> 64 bytes at most
 
 #define TABLE14_CACHE_LINE 64
